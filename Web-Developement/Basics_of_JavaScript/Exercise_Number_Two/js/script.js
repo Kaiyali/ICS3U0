389a78c1fcd2c56e062f472a6cf7ade6a55c7854
Xpliot <80181145+Kaiyali@users.noreply.github.com>
2023-03-12 19:18:01 -0700

    Working on Name_Maker

--- Web-Developement/Basics_of_JavaScript/Exercise_Number_Two/js/script.js
+++ Web-Developement/Basics_of_JavaScript/Exercise_Number_Two/js/script.js
@@ -1,5 +1,33 @@
-const info = document.querySelector(".info")
+// const word = document.querySelector(".word");
+// const sumbitBtn = document.querySelector(".submit");
+// const word_display = document.querySelector(".name-display");
 
-function changeColor(color) { 
-  info.style.color = color;
+// sumbitBtn.addEventListener("click", function(){
+//   const name = document.createElement("a");
+//   const word_display = document.createTextNode(word.value);
+//   name.appendChild(word_display)
+// });
+
+
+
+const canvas = document.querySelector(".canvas");
+const inputMessage = document.querySelector(".word");
+const displayMessage = document.createElement("div");
+const color_picker = document.querySelector(".color-picker");
+canvas.append(displayMessage);
+
+
+inputMessage.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+		event.preventDefault();
+		displayUserMessage();
+		}
+  });
+
+function displayUserMessage() {
+	displayMessage.innerHTML = inputMessage.value;
+  displayMessage.style.color = color_picker.value;
 }
+
+
+
---
+++ Web-Developement/Basics_of_JavaScript/Learning_DOM/js/script.js
@@ -0,0 +1,6 @@
+const body = document.body
+
+const div = document.createElement('div')
+div.innerText = "Daniel Kaiyali"
+div.setAttribute("class", "blue")
+body.append(div)
